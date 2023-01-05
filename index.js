64d459dca6f4c2a1858da237ea41280c644a0f16
Алексей Заикин <hydrombee@gmail.com>
2023-01-05 14:35:35 +0100

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('server working')
+import express from 'express'
+import mongoose from 'mongoose'
+
+const PORT = 5000
+const DB_URL = `mongodb+srv://user:user@cluster0.edfrxne.mongodb.net/?retryWrites=true&w=majority`
+
+const app = express()
+
+app.use(express.json())
+
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL) // connecion to mongoDB
+
+    // прослушиваем изменения на указаном порту
+    app.listen(PORT, () => {
+      console.log(`Server Started at ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startApp()
