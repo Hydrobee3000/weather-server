20097e8013a8ddb2a2c3a3a740ab3b982fd8e58a
Алексей Заикин <hydrombee@gmail.com>
2023-11-04 21:37:19 +0100

    correct path for getting all places

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ import PlaceController from './PlaceController.js' // logic of actions with plac
 const router = Router()
 
 router.post('/places', PlaceController.create) // create
-router.get('/places/', PlaceController.getAll) // read all
+router.get('/places', PlaceController.getAll) // read all
 router.get('/places/:id', PlaceController.get) // read one
 router.put('/places', PlaceController.update) // update
 router.delete('/places/:id', PlaceController.delete) // delete
